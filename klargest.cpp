c508dcdbc172c3c9545c2ab8155602857bdf7cfd
Vincent Nguyen <vincentn1102@gmail.com>
2024-05-19 19:07:26 +0930

    d

--- klargest.cpp
+++ klargest.cpp
@@ -3,7 +3,7 @@
 
 int kth_largest(std::vector<int> values, int k) {
     std::priority_queue<int> pq(values.begin(), values.end());
-    for(int i = 0; i < k - 1; ++i) {
+    for(int i = 0; i < k - 1; i++) {
         pq.pop();
     }
     return pq.top();
